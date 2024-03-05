2d3a12b95ac75177a3578aad96a53c65b5d03c8c
chuli alain <chulialain034@gmail.com>
2024-03-05 08:25:56 -0800

    file modified

---
+++ app.js
@@ -0,0 +1,7 @@
+<style> :host([hidden]),[hidden]{display:none!important} </style> == 3e
+P《style>w</style>
+o<style include-"cr-hidden-style cr-icons">@</style>
+7cstyle include-"cr-shared-style">@</style>
+rcdiv id-"content" style-"--color-new-tab-page-attribution-foregrounds rgba(32, 33,36, 1.00); --color-new-tab-page-most-visited-foreground: rgba(32, 33, 36, 1.00);-ntp-logo-color:inherit;">@《/div>(flex
+P <dom-if restamp style="display: none; ">@</dom-if>
+P<dom-if id="customizeDialogIf" restamp style="display: none;">@</dom-if>
